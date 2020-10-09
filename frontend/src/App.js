04d2a3249932f2b009fc3ac67c6a9f0f89ed0ed9
taulantruli <taulant7ruli@gmail.com>
2020-10-10 00:31:04 +0200

    Adding Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,19 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
-    <div className="App">
+    <>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
-    </div>
+    </>
   );
 };
 
