62a04501c5229ed5ec92e302467961bebed0ef26
taulantruli <taulant7ruli@gmail.com>
2020-10-20 20:33:40 +0200

    Creating Product Controller

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -90,28 +90,30 @@ const CartScreen = ({ match, location, history }) => {
         )}
       </Col>
       <Col md={4}>
-        <ListGroup variant="flush">
-          <ListGroup.Item>
-            <h2>
-              Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
-              items
-            </h2>
-            $
-            {cartItems
-              .reduce((acc, item) => acc + item.qty * item.price, 0)
-              .toFixed(2)}
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <Button
-              type="button"
-              className="btn-block"
-              disabled={cartItems.length === 0}
-              onClick={checkoutHandler}
-            >
-              Proceed to Checkout
-            </Button>
-          </ListGroup.Item>
-        </ListGroup>
+        <Card>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>
+                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                items
+              </h2>
+              $
+              {cartItems
+                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                .toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type="button"
+                className="btn-block"
+                disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
+              >
+                Proceed to Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
       </Col>
     </Row>
   );
