a466858a60324662f1d4da433f2a4400e171b11b
taulantruli <taulant7ruli@gmail.com>
2020-11-07 23:23:02 +0100

    Create Product & Edit Product Screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,6 +5,7 @@ import {
   productListReducer,
   productDetailsReducer,
   productDeleteReducer,
+  productCreateReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
 import {
@@ -26,6 +27,7 @@ import {
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  productCreate: productCreateReducer,
   productDelete: productDeleteReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
